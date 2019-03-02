45ee28dbf90fd4542c2f1b844c889d2bd969cca5
Andrew Walker <Andrew_Walker2@baylor.edu>
2019-03-02 09:38:59 -0600

    Connects to the database

--- src/main/java/api/component/DBSeeder.java
+++ src/main/java/api/component/DBSeeder.java
@@ -24,38 +24,6 @@ public class DBSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-        this.seriesRepository.deleteAll();
-
-        int count = 0;
-        File dir = new File("data");
-
-        File[] files = dir.listFiles(new FilenameFilter() {
-            public boolean accept(File dir, String filename)
-            { return filename.endsWith(".json"); }
-        } );
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        for(File f : files){
-            System.out.println(f.toString());
-            Series series = null;
-            try {
-                series = mapper.readValue(f, Series.class);
-            } catch (IOException e){
-                System.err.println("Error loading JSON file");
-            }
-
-            if(series != null){
-                System.out.println("Successfully loaded " + series.getTitle());
-                this.seriesRepository.save(series);
-                System.out.println("Successfully saved " + series.getTitle());
-                count++;
-
-            }
-        }
-
-        System.out.println("Processed " + count + " JSON files");
-
+        // Implement adding default users
     }
 }
